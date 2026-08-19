2f68a397e7ad904b217ca42a46acd8449848a5f1
LAB12_USER ON WS09 <L12X16W09@LabHCI.net>
2026-08-19 17:57:52 +0800

    Add Gradient Background

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -5,6 +5,12 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.blue,
+              Colors.pinkAccent
+            ])
+          ),
           child: Center(
             child: Text("Hello World"))),
       ),
