9ba4292e9a8cdb8b2fd5688797446ff7703a4720
Vinnie Paul <vinniepauldano@gmail.com>
2026-09-06 10:08:06 +0800

    starting to finish first quiztion (still placeholders)

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -20,10 +20,7 @@ void main() {
 
                 const Text(
                   'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
+                  style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
 
                 const SizedBox(height: 20),
@@ -34,17 +31,12 @@ void main() {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                            builder: (context) => const Quiz(),
-                          ),
+                          MaterialPageRoute(builder: (context) => const Quiz()),
                         );
                       },
                       child: const Text(
                         'Start Quiz',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
+                        style: TextStyle(fontSize: 15, color: Colors.white),
                       ),
                     );
                   },
@@ -64,9 +56,72 @@ class Quiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('yaba daba testing'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.deepPurple, Colors.purple]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //question
+              const Text(
+                'Question 1: yaba?',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 30),
+
+              //ans1
+              TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const Quiz2()),
+                  // );
+                },
+                child: const Text('daba', style: TextStyle(color: Colors.white)),
+              ),
+
+              //ans2
+              TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const Quiz2()),
+                  // );
+                },
+                child: const Text('doo', style: TextStyle(color: Colors.white)),
+              ),
+
+              //ans3
+              TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const Quiz2()),
+                  // );
+                },
+                child: const Text('daba dee', style: TextStyle(color: Colors.white)),
+              ),
+
+              //ans4
+              TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const Quiz2()),
+                  // );
+                },
+                child: const Text('daba daba', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+// copy&paste the quiz1 up top and change it to quiz2
